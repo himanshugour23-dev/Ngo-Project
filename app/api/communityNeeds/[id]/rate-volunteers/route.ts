a2299ca9c26771671f4c40539c8ee31131821b38
himanshugour23-dev <sinsinati232005@gmail.com>
2026-07-09 14:36:12 +0530

    fixing prisma transection TS error

--- app/api/communityNeeds/[id]/rate-volunteers/route.ts
+++ app/api/communityNeeds/[id]/rate-volunteers/route.ts
@@ -1,6 +1,7 @@
 import {NextResponse, NextRequest} from 'next/server';
 import {getNgoFromToken} from '@/lib/ngo-auth';
 import {prisma} from '@/lib/prisma';
+import { Prisma } from "@prisma/client";
 
 export async function GET(req: NextRequest, context: {params: Promise<{id: string}>}) {
     try{
@@ -98,7 +99,7 @@ export async function POST(req: NextRequest,context: {params: Promise<{id: strin
       );
     }
     await prisma.$transaction(
-      async (tx) => {
+      async (tx : Prisma.TransactionClient ) => {
         for (const item of ratings) {
           const {assignmentId,rating} = item;
           if ( rating !== undefined && (typeof rating !=="number" ||rating < 1 ||rating > 5)) {
